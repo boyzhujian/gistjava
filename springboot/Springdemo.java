ecc0fe24411e5864d7109047d658b7d73190cce3
Damon <cisco.damon@gmail.com>
2018-08-02 13:50:14 +0800

    Update Springdemo.java

--- springboot/Springdemo.java
+++ springboot/Springdemo.java
@@ -8,6 +8,8 @@ public class Application {
 
     public static void main(String[] args) throws Throwable {
         SpringApplication.run(Application.class, args);
+   
+        //https://docs.spring.io/spring-boot/docs/2.0.3.RELEASE/api/
     }
 
 }
